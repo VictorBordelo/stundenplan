3c2f979d61ac1b56994f4300379744f0574046c1
unknown <bepe@.(none)>
2012-11-06 14:56:02 +0100

    bat's

---
+++ cleanstarthsqlserver.bat
@@ -0,0 +1,3 @@
+del hsqltest-db.*
+"C:\Program Files (x86)\Java\jre7\bin\java" -cp C:\wamp\www\edu-hm-cs-boettcher-webtechniken\libs\hsqldb-2.2.9.jar org.hsqldb.server.Server --database.0 file:hsqltest-db --dbname.0 plandb
+pause
---
+++ starthsqlserver.bat
@@ -0,0 +1,1 @@
+"C:\Program Files (x86)\Java\jre7\bin\java" -cp C:\wamp\www\edu-hm-cs-boettcher-webtechniken\libs\hsqldb-2.2.9.jar org.hsqldb.server.Server --database.0 file:hsqltest-db --dbname.0 plandb
